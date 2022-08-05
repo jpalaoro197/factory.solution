e9c614c7bf9a0aaca62c297959e5cb862ef5c76f
jacobpalaoro <jpalaoro197@gmail.com>
2022-08-05 12:04:18 -0700

    add model references to model context file.

--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -4,7 +4,10 @@ namespace Factory.Models
 {
   public class FactoryContext : DbContext
   {
-    
+    public DbSet<Machine> Machines { get; set; }
+    public DbSet<Engineer> Engineers { get; set; }
+    public DbSet<EngineerMachine> EngineerMachine { get; set; }
+
 
     public FactoryContext(DbContextOptions options) : base(options) { }
 
