efcd6e46d62f67144e1fa5b67f227b10dda7594f
jacobpalaoro <jpalaoro197@gmail.com>
2022-08-05 12:18:33 -0700

    home controller and correct namespace in engineer controller.

--- Factory/controllers/EngineerController.cs
+++ Factory/controllers/EngineerController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System;
 
-namespace UniversityRoster.Controllers
+namespace Factory.Controllers
 {
   public class EngineersController : Controller
   {
