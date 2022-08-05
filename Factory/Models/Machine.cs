c5fb52262d1c51c765321d16579f431c792f0700
jacobpalaoro <jpalaoro197@gmail.com>
2022-08-05 10:48:28 -0700

    add machine model.

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace UniversityRoster.Models
+{
+  public class Machine
+    {
+        public Machine()
+        {
+            this.JoinEntities = new HashSet<MachineEngineer>();
+        }
+
+        public int MachineId { get; set; }
+        public string Name { get; set; }
+        
+        public virtual ICollection<MachineEngineer> JoinEntities { get; set; }
+
+    }
+}
